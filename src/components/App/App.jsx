85e21dd661a726ca34deea78ed04ac5312586005
dilaraydmr <dilaraaydemir1@gmail.com>
2025-09-24 14:02:27 +0300

    hata düzeltme

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,35 +1,38 @@
-import React, { useEffect } from 'react';
-import ContactsForm from "../ContactsForm/ContactsForm";
-import SearchBox from "../SearchBox/SearchBox";
-import ContactList from "../ContactList/ContactList";
-import { useSelector, useDispatch } from 'react-redux';
-import { selectContacts, loadContacts } from '../../redux/contactsSlice';
-import { selectNameFilter } from '../../redux/filtersSlice';
+import React, { useEffect } from "react";
+import ContactsForm from "../ContactsForm/ContactsForm.jsx";
+import SearchBox from "../SearchBox/SearchBox.jsx";
+import Contact from "../Contact/Contact.jsx";
+import { useSelector, useDispatch } from "react-redux";
+import { selectContacts, loadContacts } from "../../redux/contactsSlice";
+import { selectNameFilter } from "../../redux/filtersSlice";
 
 function App() {
   const dispatch = useDispatch();
   const contacts = useSelector(selectContacts);
   const filter = useSelector(selectNameFilter);
 
+  // İlk açılışta kontakları yükle
   useEffect(() => {
-    if (contacts.length === 0) {
-      dispatch(loadContacts());
-    }
-  }, [dispatch, contacts.length]);
+    dispatch(loadContacts());
+  }, [dispatch]);
 
   const filteredContacts = contacts.filter(contact =>
     contact.name.toLowerCase().includes(filter.toLowerCase())
   );
 
   return (
-    <div style={{ padding: '20px' }}>
+    <div style={{ padding: "20px" }}>
       <h1>Communication Book</h1>
       <ContactsForm />
       <SearchBox />
       <h2>Directory</h2>
-      {filteredContacts.map(contact => (
-        <Contact key={contact.id} contact={contact} />
-      ))}
+      {filteredContacts.length > 0 ? (
+        filteredContacts.map(contact => (
+          <Contact key={contact.id} contact={contact} />
+        ))
+      ) : (
+        <p>No contacts found</p>
+      )}
     </div>
   );
 }
